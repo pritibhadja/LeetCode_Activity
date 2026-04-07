ef6d123553900dcc588e05546965a915e8341d41
pritibhadja <pritibhadja4@gmail.com>
2026-04-07 11:48:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,14 +1,15 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         temp = n
-        sum = 0
-        product = 1
+        sum = 0 # initial sum is 0
+        product = 1 # initial product is 1
 
+        # Example = 234
         while temp > 0:
-            result = temp % 10
-            temp //= 10
+            result = temp % 10 # result = 4 # 23 % 10 =3
+            temp //= 10 # 23 # 2
 
-            sum += result
-            product *= result
+            sum += result # 0+4 = 4 # 4+3 = 7
+            product *= result # 1*4 = 4 # 4*3 = 12
 
         return product - sum
