c1fd9d4eddc1effec5454bb528002ee2fd8228e1
pritibhadja <pritibhadja4@gmail.com>
2026-04-07 11:26:33 +0530

    Time: 5 ms (71.52%), Space: 19.2 MB (67.86%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -12,8 +12,8 @@ class Solution:
             rev = rev*10 + digit # 0*10 + 1 = 1
             x //= 10 # remaining 12
 
-        if original == rev: 
-            return True
+        return original == rev
+            
 
         
         
